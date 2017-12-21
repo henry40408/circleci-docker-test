40fb836fbf7887c94fd0d2a873b1022009d11315
Heng-Yi Wu <henry40408@gmail.com>
2017-12-21 11:53:43 +0800

    feat(circleci): Use localhost instead of DOCKER_HOST IP address

--- test/nginx-with-privileged-port.sh
+++ test/nginx-with-privileged-port.sh
@@ -2,8 +2,4 @@
 
 docker run -d -p 80:80 nginx
 
-IP_ADDRESS=$(echo ${DOCKER_HOST} | egrep -o '([0-9]{1,3}[.]){3}[0-9]{1,3}')
-
-echo "Docker IP address: ${IP_ADDRESS}"
-
-curl http://${IP_ADDRESS}/
+curl http://localhost/
