fecd1f3c77699d84c161315b2ca929d4834c473a
Heng-Yi Wu <henry40408@gmail.com>
2017-12-21 11:42:05 +0800

    feat(circleci): Properly extract IP address from DOCKER_HOST

--- test/nginx-with-privileged-port.sh
+++ test/nginx-with-privileged-port.sh
@@ -1,8 +1,8 @@
-#!/bin/bash -eo pipefail
+#!/bin/bash -exo pipefail
 
 docker run -d -p 80:80 nginx
 
-IP_ADDRESS=$(echo ${DOCKER_HOST} | grep '^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$')
+IP_ADDRESS=$(echo ${DOCKER_HOST} | egrep -o '([0-9]{1,3}[.]){3}[0-9]{1,3}')
 
 echo "Docker IP address: ${IP_ADDRESS}"
 
