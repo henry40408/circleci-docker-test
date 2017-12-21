5737c77a4f21c9b89388c72ccce3b324e7ce3251
Heng-Yi Wu <henry40408@gmail.com>
2017-12-21 11:34:25 +0800

    feat(circleci): Show IP address of remote Docker

--- test/nginx-with-privileged-port.sh
+++ test/nginx-with-privileged-port.sh
@@ -4,4 +4,6 @@ docker run -d -p 80:80 nginx
 
 IP_ADDRESS=$(echo ${DOCKER_HOST} | grep '^([0-9]{1,3}\.){3}[0-9]{1,3}(\/([0-9]|[1-2][0-9]|3[0-2]))?$')
 
+echo "Docker IP address: ${IP_ADDRESS}"
+
 curl http://${IP_ADDRESS}/
